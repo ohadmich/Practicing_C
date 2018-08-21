87262dd0acf08e032dc4af4b16248d7275980d9a
ohadmich <41272397+ohadmich@users.noreply.github.com>
2018-08-20 20:58:29 -0700

    A fast working version using stack

--- Linked_lists/Friends_list/main.c
+++ Linked_lists/Friends_list/main.c
@@ -97,30 +97,35 @@ void printList(struct Node* head)
 
 // create a stack for Node pointers
 struct Node* listStack[100000] = {};
-int top = 0;
+int top = -1;
 
+// push to stack function
 void push(struct Node* node)
 {
     top = top+1;
     listStack[top] = node;
 }
 
+// a function to check if stack is empty
 bool isEmpty()
 {
     return top == -1;
 }
 
+// pop element off of stack
 void pop()
 {
     if (isEmpty()) return;
     else top = top-1;
 }
 
+// returns the top element in the stack
 struct Node* topElement()
 {
     return listStack[top];
 }
 
+// initializes the stack
 void intstack()
 {
     top = -1;
@@ -137,39 +142,42 @@ int main()
         // read number of friends, N, and who many of them should be deleted, K
         scanf("%d", &N);
         scanf("%d", &K);
-        // read list of popularity values into our linked list
+        /**** read list of popularity values into the linked list****/
         while(N--)
         {
             scanf("%d", &temp);
             addNode(&head, temp);
         }
-        // reverse list
+        // reverse list to make the next step easier
         head = revlist(head);
-        // delete K freinds
+        /**** delete K freinds for which the next friend has higher popularity ****/
+        intstack();
+        push(head);
         while(K--)
         {
-            bool deleteFriend = 0;
-            // first check if the head should be deleted
-            if(head->next->data > head->data)
+            struct Node* current = topElement();
+            // keep in stack every node that obeys the rule
+            while(current->next != NULL && current->data >= current->next->data)
+            {
+                current = current->next;
+                push(current);
+            }
+            // pop the node that doesn't obey the rule
+            pop();
+            // if the stack is empty have to delete the head
+            if (isEmpty())
             {
                 delNode(&head,1);
-                continue;
+                push(head);
             }
-            // check the the rest of the list
-            struct Node* current = head;
-            while(current->next->next != NULL)
+            // else delete intermediate node
+            else
             {
-                if( (current->next->next->data) > (current->next->data) )
-                {
-                    delNode(&current,0);
-                    deleteFriend = 1;
-                    break;
-                }
-                current = current->next;
+                current = topElement();
+                delNode(&current,0);
             }
-            // delete last node if we didn't find any node to delete
-            if(!deleteFriend) delNode(&current,0);
         }
+        // print the resulting list
         printList(head);
         printf("\n");
     }
