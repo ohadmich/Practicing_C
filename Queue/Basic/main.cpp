24d0d49259d72dba9573051c96d5f2c34caa8cbc
ohadmich <41272397+ohadmich@users.noreply.github.com>
2018-08-22 08:55:45 -0700

    added some comments

--- Queue/Basic/main.cpp
+++ Queue/Basic/main.cpp
@@ -1,9 +1,12 @@
+/* A basic program to practice queues */
+
 #include <iostream>
 using namespace std;
 #define Nmax 100
 
 int queue[Nmax]={}, front = 0, rear = 0; 
 
+// a function for adding an element to the queue from the rear
 void enqueue(int num)
 {
     if (rear == Nmax-1 )
@@ -15,6 +18,7 @@ void enqueue(int num)
     rear = rear+1;
 }
 
+// a function for deleting an element from the front
 void dequeue()
 {
     if (front == rear)
@@ -26,6 +30,7 @@ void dequeue()
     front = front+1;
 }
 
+// returns the front element
 int frontElement()
 {
     return queue[front];
@@ -35,18 +40,23 @@ int main()
 {
     int N, temp;
     char ch;
+    // read N, number of operations
     cin >> N;
     while(N--)
     {
+        // read the char symbol that represents the action needed to be done
         cin >> ch;
+        // if 'E' was recieved, enque the next number and print the length of the queue
         if (ch == 'E')
         {
             cin >> temp;
             enqueue(temp);
             cout << rear - front << "\n";
         }
+        // if 'D' was recieved, print the front element, dequeue it and print the length of the queue
         else if (ch == 'D')
         {
+            // if the queue is empty, print -1
             if (front == rear) cout << -1 << " ";
             else 
             {
