fdfdde951d5b629bff49207c00b6d8ca45823807
ohadmich <41272397+ohadmich@users.noreply.github.com>
2018-08-31 09:11:45 -0700

    Added more comments

--- Tree/Same_color/main.cpp
+++ Tree/Same_color/main.cpp
@@ -1,9 +1,15 @@
+/*******************************************************************************************************
+For a tree with N nodes where each node has one color out of C different colors, print for each node the
+closest ancestor with the same color. If it does not exist, print -1.
+********************************************************************************************************/
+
 #include <iostream>
 using namespace std;
 #define Nmax 100000
 
 int main()
 {
+    /* Read inputs */
     int N, C;
     int parents[Nmax]={}, color[Nmax]={};
     // Read Number of nodes and number of colors
@@ -21,15 +27,21 @@ int main()
     }
 
     /* Analysis */
-    cout << -1 << " "; // The root has no ancestors with the same color
+    // The root has no ancestors with the same color, so print -1
+    cout << -1 << " ";
+    // Traverse the tree nodes
     for(int i=2; i<N+1; i++)
     {
-        int current = parents[i]; 
+        // Initialize current to be the parent of the node in question
+        int current = parents[i];
+        // Move from one parent to the next until you find the same color or pass the root 
         while(current != 0 && color[current] != color[i])
         {
             current = parents[current];
         }
+        // Print the node number of the closest parent with same color
         if(current) cout << current << " ";
+        // Print -1 if it does not exist
         else cout << -1 << " ";
     }
     return 0;
