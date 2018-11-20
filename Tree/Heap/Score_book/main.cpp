1d47be346d24c7a82dbf39525d4997c457384659
ohadmich <41272397+ohadmich@users.noreply.github.com>
2018-11-19 23:11:01 -0800

    A working faster version

--- Tree/Heap/Score_book/main.cpp
+++ Tree/Heap/Score_book/main.cpp
@@ -10,18 +10,23 @@
 using namespace std;
 #define L int(1e5 + 1)
 
-// Define arrays for max and min heaps
-int Amax[L] = {}, Amin[L] = {}, N = 0;
+// Define arrays for max and min heaps and a frequency array to keep account for repeating scores
+int Amax[L] = {}, Amin[L] = {}, Freq[L] = {}, Nmin = 0, Nmax = 0;
 
 // Print max and min heaps
 void print()
 { 
-    for(int i=1; i<=N; i++)
+    for(int i=1; i<=Nmax; i++)
     {
         cout << Amax[i] << " ";
     }
     cout << endl;
-    for(int i=1; i<=N; i++)
+    for(int i=1; i<=Nmax; i++)
+    {
+        cout << Freq[Amax[i]] << " ";
+    }
+    cout << endl;
+    for(int i=1; i<=Nmin; i++)
     {
         cout << Amin[i] << " ";
     }
@@ -32,7 +37,7 @@ void print()
 void Bubblemax(int i)
 {
     // Return if the heap is empty
-    if(N<1) return;
+    if(Nmax<1) return;
     // Define parent index
     int p = i/2;
     /* Order the heap*/
@@ -54,7 +59,7 @@ void Bubblemax(int i)
 void Bubblemin(int i)
 {
     // Return if the heap is empty
-    if(N<1) return;
+    if(Nmin<1) return;
     // Define parent index
     int p = i/2;
     /* Order the heap*/
@@ -74,44 +79,44 @@ void Bubblemin(int i)
 
 
 // Order the max heap top down
-void maxHeapify(int A[L], int n, int i)
+void maxHeapify(int i)
 {
     // Define left and right node indices
     int left = 2*i, right = 2*i +1;
     // Initialize largest to be i, then change if needed
     int largest = i;
     // If left is within the array bounds and its element is larger, change largest
-    if( left <= n && A[left] > A[largest]) largest = left;
+    if( left <= Nmax && Amax[left] > Amax[largest]) largest = left;
     // If right is within the array bounds and its element is larger, change largest
-    if(right <= n && A[right] > A[largest]) largest = right;
+    if(right <= Nmax && Amax[right] > Amax[largest]) largest = right;
     // If i is not the largest element, swap places and then check the new subtree
     if(largest != i)
     {
-        int temp = A[i];
-        A[i] = A[largest];
-        A[largest] = temp;
-        maxHeapify(A, n, largest);
+        int temp = Amax[i];
+        Amax[i] = Amax[largest];
+        Amax[largest] = temp;
+        maxHeapify(largest);
     }
 }
 
 // Order the min heap top down
-void minHeapify(int A[L], int n, int i)
+void minHeapify(int i)
 {
     // Define left and right node indices
     int left = 2*i, right = 2*i +1;
     // Initialize smallest to be i, then change if needed
     int smallest = i;
     // If left is within the array bounds and its element is smaller, change smallest
-    if( left <= n && A[left] < A[smallest]) smallest = left;
+    if( left <= Nmin && Amin[left] < Amin[smallest]) smallest = left;
     // If right is within the array bounds and its element is smaller, change smallest
-    if(right <= n && A[right] < A[smallest]) smallest = right;
+    if(right <= Nmin && Amin[right] < Amin[smallest]) smallest = right;
     // If i is not the smallest element, swap places and then check the new subtree
     if(smallest != i)
     {
-        int temp = A[i];
-        A[i] = A[smallest];
-        A[smallest] = temp;
-        minHeapify(A, n, smallest);
+        int temp = Amin[i];
+        Amin[i] = Amin[smallest];
+        Amin[smallest] = temp;
+        minHeapify(smallest);
     }
 }
 
@@ -119,135 +124,99 @@ void minHeapify(int A[L], int n, int i)
 void Push(int X)
 {
     // Add element to max heap and min heap
-    N = N+1;
-    Amax[N] = X;
-    Amin[N] = X;
+    Nmax++;
+    Nmin++;
+    Amax[Nmax] = X;
+    Amin[Nmin] = X;
+    // Update frequency array
+    Freq[X]++;
     // Order heaps
-    Bubblemax(N);
-    Bubblemin(N);
+    Bubblemax(Nmax);
+    Bubblemin(Nmin);
 
 }
 // Find the difference between the largest and smallest elements
 void Diff()
 {
+    // Clear elements with zero frequency from heaps
+    while(Nmax>0 && Freq[Amax[1]] == 0)
+    {
+        Amax[1] = Amax[Nmax];
+        Nmax--;
+        maxHeapify(1);
+    }
+    while(Nmin>0 && Freq[Amin[1]] == 0)
+    {
+        Amin[1] = Amin[Nmin];
+        Nmin--;
+        minHeapify(1);
+    }
     // When the array is empty print -1
-    if(N == 0){
+    if(Nmax == 0 || Nmin == 0){
         cout << -1 << endl;
         return;
     }
     // When there is only one element in the array, print zero and reduce N to 0  
-    if(N == 1){
+    if(Amax[1] == Amin[1]){
         cout << 0 << endl;
-        N = N-1;
+        Freq[Amax[1]]--;
         return;
     }
     // Compute max - min difference
-    int maxEl = Amax[1], minEl = Amin[1];
-    int diff = maxEl - minEl;
+    int diff = Amax[1] - Amin[1];
     // Print diff
     cout << diff << endl;
-    // Delete top elements
-    Amax[1] = Amax[N];
-    Amax[N] = 0;
-    Amin[1] = Amin[N];
-    Amin[N] = 0;
-    N = N - 1;
+    // Reduce frequency of min and max elements
+    Freq[Amax[1]]--;
+    Freq[Amin[1]]--;
+    // Delete min and max elements
+    Amax[1] = Amax[Nmax];
+    Amin[1] = Amin[Nmin];
+    Nmax--;
+    Nmin--;
     // Order heaps
-    maxHeapify(Amax, N, 1);
-    minHeapify(Amin, N, 1);
-    // Delete leaf from max heap
-    int imin = N/2 +1, imax = N/2 +1;
-    while(imin <= N)
-    {
-        if(Amax[imin] == minEl) break;
-        imin++;
-    }
-    while(imin <= N)
-    {
-        Amax[imin] = Amax[imin+1];
-        Bubblemax(imin);
-        imin++;
-    }
-    // Delete leaf from min heap
-    while(imax <= N)
-    {
-        if(Amin[imax] == maxEl) break;
-        imax++;
-    }
-    while(imax < N)
-    {
-        Amin[imax] = Amin[imax+1];
-        Bubblemin(imax);
-        imax++;
-    }
-    N = N-1;
-
+    maxHeapify(1);
+    minHeapify(1);
 
 }
-// Count how many instances of the largest element exist
+// Print how many instances of the largest element exist
 void CountHigh()
 {
+    // Clear elements with zero frequency from heaps
+    while(Nmax>0 && Freq[Amax[1]] == 0)
+    {
+        Amax[1] = Amax[Nmax];
+        Nmax--;
+        maxHeapify(1);
+    }
     // if the heap is empty, print -1 and return
-    if(N < 1)
+    if(Nmax < 1)
     {
         cout << -1 << endl;
         return;
     }
-    // Create a copy of the max heap
-    int A[L]={};
-    for(int i = 1; i<=N; i++)
-    {
-        A[i] = Amax[i];
-    }
-    // Initialize count and save max value
-    int count = 0, max = A[1], n = N;
-    // Pop max and reorder until you get a different max
-    while(A[1] == max)
-    {
-        // Pop max and move the last element to the top
-        A[1] = A[n];
-        A[n] = 0;
-        n = n-1;
-        // Order heap
-        maxHeapify(A, n, 1);
-        // Count
-        count++;
-    }
     // print number of max value elements
-    cout << count << endl;
+    cout << Freq[Amax[1]] << endl;
 
 }
 // Count how many instances of the smallest element exist
 void CountLow()
 {
+    // Clear elements with zero frequency from heaps
+    while(Nmin>0 && Freq[Amin[1]] == 0)
+    {
+        Amin[1] = Amin[Nmin];
+        Nmin--;
+        minHeapify(1);
+    }
     // if the heap is empty, print -1 and return
-    if(N < 1)
+    if(Nmin < 1)
     {
         cout << -1 << endl;
         return;
     }
-    // Create a copy of the min heap
-    int A[L]={};
-    for(int i = 1; i<=N; i++)
-    {
-        A[i] = Amin[i];
-    }
-    // Initialize count and save min value
-    int count = 0, min = A[1], n = N;
-    // Pop max and reorder until you get a different min
-    while(A[1] == min)
-    {
-        // Pop min and move the last element to the top
-        A[1] = A[n];
-        A[n] = 0;
-        n = n-1;
-        // Order heap
-        minHeapify(A, n, 1);
-        // Count
-        count++;
-    }
     // print number of min value elements
-    cout << count << endl;
+    cout << Freq[Amin[1]] << endl;
 }
 
 int main()
